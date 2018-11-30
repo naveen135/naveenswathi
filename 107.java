3419a78a2935111095ad9b567822b193f5a9562d
naveen135 <41731271+naveen135@users.noreply.github.com>
2018-11-30 19:44:59 +0530

    Create 107.java

---
+++ 107.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+
+class Program107
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+ 
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		int C=sc.nextInt();
+		int result=(A*B)/C;
+		System.out.println(result);
+	}
+}
+
